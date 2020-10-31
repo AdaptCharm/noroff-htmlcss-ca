0ca024cd0a5c4e891e563b7a16955adf814ec97d
AdaptCharm <jamie@jamie.no>
2020-10-31 13:14:20 +0100

    Update index route

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,15 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Page from './../components/page'
+import Footer from './../components/footer'
 
-export default function Home() {
+import Hero from './../components/hero'
+
+const Index = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Home | RainyDays</title>
-        <link rel="icon" href="/favicon.svg" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Hello <a href="/">World!</a>
-        </h1>
-
-        <p className={styles.description}>
-          This is the website of RainyDays.
-        </p>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="/"
-        >
-          <img src="/logo.png" alt="RainyDays Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    <Page>
+      <Hero />
+      <Footer />
+    </Page>
   )
 }
+
+export default Index

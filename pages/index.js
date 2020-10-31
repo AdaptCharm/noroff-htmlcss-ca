632057b82a08ff8da2ffdf704af689ab91672602
AdaptCharm <jamie@jamie.no>
2020-10-31 20:43:20 +0100

    Move hero component to index route

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
 import Page from './../components/page'
 import Footer from './../components/footer'
 
-import Hero from './../components/hero'
+const Image = ({ src, ...props }) => (
+  <figure>
+    <img src={src} {...props} />
+  </figure>
+)
 
 const Index = () => {
   return (
     <Page>
-      <Hero />
+      <section className="section hero">
+        <div className="inner side-by-side">
+          <div className="image">
+            <Image
+              src="/jackets/coral-jacket.png"
+              alt="Coral jacket"
+            />
+          </div>
+          <div className="content">
+            <h1 className="title">Waterproof rain jackets</h1>
+            <a href="" className="button">See all jackets</a>
+          </div>
+        </div>
+      </section>
       <Footer />
     </Page>
   )

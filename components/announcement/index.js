3e132dd4bf053c09a3da3795f28b5c235fb2dcf9
AdaptCharm <jamie@jamie.no>
2020-10-31 13:21:37 +0100

    Add announcement component
    TODO: Minor refactoring

---
+++ components/announcement/index.js
@@ -0,0 +1,12 @@
+const Announcement = () => {
+  return (
+    <div className="announcement">
+      <div className="inner">
+        <p>Get 10% off your next purchase! Use coupon code: <strong>DISCOUNT10</strong></p>
+        <p>Coupon code <strong>DISCOUNT10</strong> for 10% off!</p>
+      </div>
+    </div>
+  )
+}
+
+export default Announcement

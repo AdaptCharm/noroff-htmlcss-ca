44fe68092f2d6277d703505639442b1b6eb69387
AdaptCharm <jamie@jamie.no>
2020-10-31 13:56:16 +0100

    Minor refactoring

--- components/announcement/index.js
+++ components/announcement/index.js
@@ -1,9 +1,11 @@
+import styles from './announcement.module.css'
+
 const Announcement = () => {
   return (
-    <div className="announcement">
-      <div className="inner">
-        <p>Get 10% off your next purchase! Use coupon code: <strong>DISCOUNT10</strong></p>
-        <p>Coupon code <strong>DISCOUNT10</strong> for 10% off!</p>
+    <div className={styles.announcement}>
+      <div className={styles.inner}>
+        <p className={styles.text}>Get 10% off your next purchase! Use coupon code: <strong>DISCOUNT10</strong></p>
+        <p className={styles.text}>Coupon code <strong>DISCOUNT10</strong> for 10% off!</p>
       </div>
     </div>
   )

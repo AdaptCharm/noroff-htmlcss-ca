d634bf309262e19bf0268feaec94f5af1e265326
AdaptCharm <jamie@jamie.no>
2020-11-01 04:05:36 +0100

    Use the new image component 👀

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,16 @@
+import Image from 'next/image'
+
 import Page from './../components/page'
 import Footer from './../components/footer'
 
 import heroStyles from './../styles/pages/home/hero.module.css'
 
 const Index = () => {
-  const Image = ({ src, ...props }) => (
-    <figure>
-      <img src={src} {...props} />
-    </figure>
-  )
-  
   return (
     <Page>
       <section className={heroStyles.hero}>
         <div className={heroStyles.inner}>
-          <div className={heroStyles.image}>
-            <Image
-              src="/jackets/coral-jacket.png"
-              alt="Coral jacket"
-            />
-          </div>
+          <Image src="/jackets/coral-jacket.png" width="213" height="220" alt="Coral Jacket" />
           <div className={heroStyles.content}>
             <h1 className={heroStyles.title}>Waterproof rain jackets</h1>
             <a href="" className="button">See all jackets</a>
